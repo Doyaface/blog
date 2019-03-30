d1c175a7ef58407ed8ca2e50b5ffbf08f88e5c7e
Doyaface <takahiro@babatakashidainoMacBook-puro.local>
2019-03-30 17:09:11 +0900

    Conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-=======
   root 'article#index'
->>>>>>> make_index
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
