70b240525fea446ec2e0ce1792934919830fb6e5
Doyaface <takahiro@babatakashidainoMacBook-puro.local>
2019-03-30 18:56:27 +0900

    edit_new_view_and_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,18 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def new
+    @article = Article.new
+  end
+  def create
+    article = Article.new(article_params)
+    article.save!
+    redirect_to article_url, notice: "投稿しました"
+  end
+
+  private
+  def article_params
+    params.require(:article).parmit(:title,:text,:image)
+  end
 end
